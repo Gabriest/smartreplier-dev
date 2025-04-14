4d298861f3f08b3459c41cf341a2c30d88784270
Gabriest <gabrielstsmoreira.2003@gmail.com>
2025-04-14 16:51:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const session = useSession();
+  const loading = session.status === "loading";
+
+  if (loading) return <p>A carregar...</p>;
 
   return (
     <main style={{ padding: 20 }}>
       <h1>SmartReplier Dev</h1>
-      {!session && (
+
+      {!session.data ? (
         <>
           <p>Não estás autenticado.</p>
           <button onClick={() => signIn("google")}>Entrar com Google</button>
         </>
-      )}
-      {session && (
+      ) : (
         <>
-          <p>Bem-vindo, {session.user.email}</p>
-          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <p>Bem-vindo, {session.data.user.email}</p>
+          <pre>{JSON.stringify(session.data, null, 2)}</pre>
           <button onClick={() => signOut()}>Sair</button>
         </>
       )}
